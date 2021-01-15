14e9b6ad604c1fa24bfc42ebfa3177f38b6af164
kherma <kamil.herma01@gmail.com>
2021-01-15 18:48:50 +0100

    Add Header layout

---
+++ src/layout/header/header.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './header.styles.scss';
+import { ReactComponent as Logo } from './../../assets/crown.svg';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <div className="header">
+    <Link to="/" className="logo-container">
+      <Logo className="logo" />
+    </Link>
+    <div className="header-nav">
+      <Link to="/shop" className="header-link">
+        SHOP
+      </Link>
+      <Link to="/shop" className="header-link">
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
